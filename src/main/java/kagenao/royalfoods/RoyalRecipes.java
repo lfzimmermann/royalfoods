fb0cecf75f1c8c02a4f0892f6e498f13fe2f58cc
Luca Frank Zimmermann <luca-frank.zimmermann@stud.uni-bamberg.de>
2025-07-10 17:32:40 +0200

    finally found out how recipes work.

--- src/main/java/kagenao/royalfoods/RoyalRecipes.java
+++ src/main/java/kagenao/royalfoods/RoyalRecipes.java
@@ -1,14 +1,14 @@
 package kagenao.royalfoods;
 
-import net.minecraft.core.block.Blocks;
-import net.minecraft.core.data.DataLoader;
 import net.minecraft.core.data.registry.Registries;
-import net.minecraft.core.data.registry.recipe.RecipeGroup;
 import net.minecraft.core.data.registry.recipe.RecipeNamespace;
-import net.minecraft.core.data.registry.recipe.RecipeSymbol;
-import net.minecraft.core.data.registry.recipe.entry.RecipeEntryFurnace;
 import net.minecraft.core.item.ItemStack;
+import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.util.RecipeEntrypoint;
+
+import java.util.List;
+
+import static kagenao.royalfoods.RoyalFoods.LOGGER;
 
 public class RoyalRecipes implements RecipeEntrypoint {
 	public static RecipeNamespace ROYAL_FOODS = new RecipeNamespace();
@@ -17,33 +17,38 @@ public class RoyalRecipes implements RecipeEntrypoint {
 
 	@Override
 	public void onRecipesReady() {
-		RoyalFoods.LOGGER.info("Loading SI recipes...");
-		this.load();
+		LOGGER.info("Loading SI recipes...");
+		this.initializeRecipes();
 	}
 
 	@Override
 	public void initNamespaces() {
-		RoyalFoods.LOGGER.info("Loading SI recipe namespaces...");
-		this.resetGroups();
-		this.registerNamespaces();
+		LOGGER.info("Loading SI recipe namespaces...");
+		RecipeBuilder.initNameSpace("royalfoods");
+		RecipeBuilder.getRecipeNamespace("royalfoods");
+		// listItemGroups();
+		Registries.ITEM_GROUPS.register("royalfoods:item/food", Registries.stackListOf(RoyalFoods.raw_mutton, RoyalFoods.cooked_mutton));
+		LOGGER.info("SI recipe namespaces initialized.");
 	}
 
-	public void load(){
-		RoyalFoods.LOGGER.info("load...");
-		DataLoader.loadRecipesFromFile("/assets/royalfoods/recipes/furnace.json");
-		RoyalFoods.LOGGER.info("finished loading...");
-		RoyalFoods.LOGGER.info("{} recipes in {} groups.", ROYAL_FOODS.getAllRecipes().size(), ROYAL_FOODS.size());
+	private static void listItemGroups() {
+		for (List<ItemStack> is : Registries.ITEM_GROUPS){
+			LOGGER.info(Registries.ITEM_GROUPS.getKey(is));
+		}
+	}
 
+	public void initializeRecipes(){
+		RecipeBuilder.Furnace("royalfoods")
+			.setInput(RoyalFoods.raw_mutton)
+			.create("raw_mutton_to_cooked_mutton", RoyalFoods.cooked_mutton.getDefaultStack());
+
+		LOGGER.info("Initialized recipes.");
 	}
 
 	public void registerNamespaces() {
-		Registries.RECIPES.register("royalfoods", ROYAL_FOODS);
-		(new RecipeGroup<>(new RecipeEntryFurnace().getInput()).
 	}
 
 	public void resetGroups() {
-		Registries.RECIPES.unregister("royalfoods");
-		ROYAL_FOODS = new RecipeNamespace();
 	}
 
 }
